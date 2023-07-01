c61df8b71ccc4841373db7958a8fca4cf16d060b
Suvarnkranti Birajdar <112853118+Suvarnkrantibirajdar@users.noreply.github.com>
2023-07-01 18:57:40 +0530

    Add files via upload

---
+++ EmployeeDTOProject/src/main/java/com/spring/controller/ApploicationContainer.java
@@ -0,0 +1,44 @@
+package com.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.entity.IdPass;
+import com.spring.jwt.CustomUserDetails;
+import com.spring.jwt.JwtUtil;
+
+
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class ApploicationContainer {
+
+	@Autowired CustomUserDetails cu;
+	@Autowired JwtUtil jwt;
+	@Autowired DaoAuthenticationProvider dao;
+	
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@RequestBody IdPass id)
+	{
+		try
+		{
+			this.dao.authenticate((new UsernamePasswordAuthenticationToken(id.getUsername(), id.getPassword())));
+			
+		}
+		catch(Exception e)
+		{
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+		UserDetails usedDetals=this.cu.loadUserByUsername(id.getUsername());
+		
+		return ResponseEntity.ok(jwt.generateToken(usedDetals));
+	}
+}
---
+++ EmployeeDTOProject/src/main/java/com/spring/entity/IdPass.java
@@ -0,0 +1,14 @@
+package com.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IdPass {
+
+	private String username;
+	private String password;
+}
